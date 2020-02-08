47962c1fb96b6db35c95da10ec3a9be55d62d195
vagrant <vagrant@homestead>
2020-02-08 11:50:21 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,23 +6,24 @@ use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+// Model Factories
 
+// 每个模型工厂定义
+// 工厂提供了一种方便的方法来生成新的模型实例 用来生成填充数据
+
+
+// factory的define(Eloquent 模型类, 闭包函数) 定义模型工厂
 $factory->define(User::class, function (Faker $faker) {
+    // Faker 方法来生成假数据 => 模型的指定字段赋值
+    $date_time = $faker->date . ' ' . $faker->time; // 当前时间
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'introduction' => $faker->sentence(), // 随机生成小段落
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });

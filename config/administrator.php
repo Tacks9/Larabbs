154281c4c310d5d76c2d7486d56ca80929f2c114
vagrant <vagrant@homestead>
2020-03-03 07:58:55 +0000

    添加标签功能-添加后台标签管理

--- config/administrator.php
+++ config/administrator.php
@@ -79,6 +79,7 @@ return array(
         ],
         '内容管理' => [
             'categories',
+            'tags',
             'topics',
             'replies',
         ],
---
+++ config/administrator/tags.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\Models\Tag;
+
+return [
+    'title'   => '标签',
+    'single'  => '标签',
+    'model'   => Tag::class,
+
+    // 对 CRUD 动作的单独权限控制，其他动作不指定默认为通过
+    'action_permissions' => [
+        // 删除权限控制
+        'delete' => function () {
+            // 只有站长才能删除话题分类
+            return Auth::user()->hasRole('Founder');
+        },
+    ],
+
+    'columns' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'name' => [
+            'title'    => '名称',
+            'sortable' => false,
+        ],
+        'description' => [
+            'title'    => '关键字',
+            'sortable' => false,
+        ],
+        'post_count' => [
+            'title'    => '帖子数量',
+        ],
+        'operation' => [
+            'title'  => '管理',
+            'sortable' => false,
+        ],
+    ],
+    'edit_fields' => [
+        'name' => [
+            'title' => '名称',
+        ],
+        'description' => [
+            'title' => '描述',
+            'type'  => 'textarea',
+        ],
+    ],
+    'filters' => [
+        'id' => [
+            'title' => '分类 ID',
+        ],
+        'name' => [
+            'title' => '名称',
+        ],
+        'description' => [
+            'title' => '描述',
+        ],
+    ],
+    'rules'   => [
+        'name' => 'required|min:1|unique:tags'
+    ],
+    'messages' => [
+        'name.unique'   => '分类名在数据库里有重复，请选用其他名称。',
+        'name.required' => '请确保名字至少一个字符以上',
+    ],
+];
